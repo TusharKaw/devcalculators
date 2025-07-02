d7dea32191b2bd1744b0bb34ad396812773e9969
TusharKaw <108132018+TusharKaw@users.noreply.github.com>
2025-07-02 23:39:41 +0530

    removed redunant files

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ToolsPageClient } from "@/components/tools-page-client"
-
-export default function Home() {
-  return (
-    <div className="bg-gray-50">
-      <ToolsPageClient />
-    </div>
-  )
-}
--- app/tools/json-to-csv/page.tsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function JSONToCSVRedirect() {
-  redirect("/tools/csv-json-converter/json-to-csv")
-} 
